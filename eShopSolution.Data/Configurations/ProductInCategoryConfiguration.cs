a6e3c03dd352772c565336f9f509ecb4d227b2bd
nguyenthahnhan98 <nguyenthahnhan98@gmail.com>
2021-11-09 23:42:18 +0700

    Add Configuration

---
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.ToTable("ProductInCategories");
+            builder.HasKey(t => new { t.CategoryId, t.ProductId });
+            builder.HasOne(p => p.Product).WithMany(pr => pr.ProductInCategories)
+                .HasForeignKey(pr => pr.ProductId);
+            builder.HasOne(c => c.Category).WithMany(pc => pc.ProductInCategories)
+                .HasForeignKey(pc => pc.CategoryId);
+        }
+    }
+}
