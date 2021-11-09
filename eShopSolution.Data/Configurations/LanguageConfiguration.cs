c8d6ea4e6406cdb08fe1b34309232e61024f07c3
nguyenthahnhan98 <nguyenthahnhan98@gmail.com>
2021-11-10 00:58:27 +0700

    Migration Database

--- eShopSolution.Data/Configurations/LanguageConfiguration.cs
+++ eShopSolution.Data/Configurations/LanguageConfiguration.cs
@@ -11,7 +11,7 @@ namespace eShopSolution.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<Language> builder)
         {
-            builder.ToTable("languages");
+            builder.ToTable("Languages");
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).IsRequired().IsUnicode(false).HasMaxLength(5);
             builder.Property(x => x.Name).IsRequired().HasMaxLength(30);
